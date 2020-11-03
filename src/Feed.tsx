42be36be772b49febf1d243627f8dbeab5d2bc51
Ghislain Desfossés <ghislain.desfosses@hec.edu>
2020-11-03 20:02:11 +0100

    v1 of the API call - working

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -2,42 +2,49 @@ import React, { Component } from "react";
 import FeedPersonElement from "./FeedPersonElement.tsx";
 
 class Feed extends Component {
+  // private keyword does not work
+  queryUrlAPI: string =
+    "https://api.unsplash.com/photos?query=person&count=10&client_id=xqlm32QRXwUNWsYKSXoNhKj9dKvE83hGgRNHK32H2d4";
+
   constructor() {
     super();
+
+    this.state = {
+      peopleFeedInfo: [],
+    };
   }
 
-  /* componentDidMount() {
-      const queryUrlAPI = "https://api.unsplash.com/photos?query=person&count=10&client_id=xqlm32QRXwUNWsYKSXoNhKj9dKvE83hGgRNHK32H2d4";
-      fetch(this.queryUrlAPI)
-      //  .then(res => res)
-        .then(result =>
-          console.log(result)
-        )
-    }*/
+  hydratePeopleFeedInfo(photos: object): any {
+    let possibleNames = ["Sophie", "Jasmine", "Emilie", "Alice"];
+    let peopleFeedInfo = [];
+
+    if (typeof photos !== "undefined")
+      photos.map((photo) => {
+        let personInfo = new Object();
+        personInfo["name"] =
+          possibleNames[Math.floor(Math.random() * possibleNames.length)];
+        personInfo["image"] = photo.urls.small;
+        peopleFeedInfo.push(personInfo);
+      });
+
+    this.setState({
+      peopleFeedInfo: peopleFeedInfo,
+    });
+  }
+
+  componentDidMount() {
+    fetch(this.queryUrlAPI)
+      .then((res) => res.json())
+      .then((result) => this.hydratePeopleFeedInfo(result));
+
+    //manage possible errors, and check the status code before...
+  }
 
   render(): JSX.Element {
-    const peopleFeedInfo = [
-      {
-        name: "Sophie",
-        image:
-          "https://pbs.twimg.com/media/DfkhrO1XUAEYkdw?format=jpg&name=small",
-      },
-      {
-        name: "Jasmine",
-        image:
-          "https://pbs.twimg.com/media/DfkhrO1XUAEYkdw?format=jpg&name=small",
-      },
-      {
-        name: "Emilie",
-        image:
-          "https://pbs.twimg.com/media/DfkhrO1XUAEYkdw?format=jpg&name=small",
-      },
-    ];
-
     return (
       <div id="feed">
-        {peopleFeedInfo.map((person) => (
-          <FeedPersonElement key={person.name} personInfo={person} />
+        {this.state.peopleFeedInfo.map((person) => (
+          <FeedPersonElement key={person.image} personInfo={person} />
         ))}
       </div>
     );
